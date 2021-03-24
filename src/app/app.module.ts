4ef4f319ad4c27dc68af12c4eddfc0b4ec4a5328
Nikhil <rockrajnikhil@gmail.com>
2021-03-24 22:25:37 +0530

    first commit !

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { SlideshowComponent } from './welcome-page/slideshow/slideshow.component';
+import { ItemsComponent } from './welcome-page/items/items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    WelcomePageComponent,
+    SlideshowComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
